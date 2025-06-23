d2ec5958dc628e312668e2b17785304fc667d956
pro_c <hieutg244@gmail.com>
2025-06-23 23:19:44 +0700

    space hunter

--- Assets/_Game/ChuongScripts/Scripts/UI/LevelManager/InfoMapItem.cs
+++ Assets/_Game/ChuongScripts/Scripts/UI/LevelManager/InfoMapItem.cs
@@ -3,17 +3,17 @@ using UnityEngine.UI;
 
 namespace Game
 {
-    public class InfoMapItem : MonoBehaviour
-    {
-        [SerializeField] private GameObject _lock;
-        [SerializeField] private Image _iconImg;
+	public class InfoMapItem : MonoBehaviour
+	{
+		[SerializeField] private GameObject _lock;
+		[SerializeField] private Image      _iconImg;
 
-        public void SetInfo(Sprite iconMap, bool isLock)
-        {
-            _iconImg.sprite = iconMap;
-            _iconImg.SetNativeSize();
-            if (_lock)
-                _lock.SetActive(isLock);
-        }
-    }
+		public void SetInfo(Sprite iconMap,bool isLock)
+		{
+			_iconImg.sprite=iconMap;
+			_iconImg.SetNativeSize();
+			if(_lock)
+				_lock.SetActive(isLock);
+		}
+	}
 }
